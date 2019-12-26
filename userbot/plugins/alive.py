0c79038780e1a8190285077517d4f924dd16d089
Opgohil <59160477+gohilbapu@users.noreply.github.com>
2019-12-26 09:50:23 +0530

    Update alive.py

--- userbot/plugins/alive.py
+++ userbot/plugins/alive.py
@@ -6,12 +6,12 @@ from platform import uname
 from userbot import ALIVE_NAME
 from userbot.utils import admin_cmd
 
-DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "♔彡[ GUJJUOPGOHIL ]彡♔, check pinned in opgohil"
+DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "No name set yet nibba, check pinned in @XtraTgBot"
 
 @command(outgoing=True, pattern="^.alive$")
 async def amireallyalive(alive):
     """ For .alive command, check if the bot is running.  """
     await alive.edit("**`YO BRO JABTAK YE KHEL KHATAM NHI HOTA APUN IDHARICH HAI ψ(｀∇´)ψ`**\n\n"
-                     "`Telethon version: 6.9.0\nPython: 3.7.3\nfork by:` @opgohil\n`Database Status: Databases functioning normally!\n\nAlways with you, my master!\n`"
-                     f"`My peru owner`: {opgohil}\n"
+                     "`Telethon version: 6.9.0\nPython: 3.7.3\nfork by:` @gujjuopgohil\n`Database Status: Databases functioning normally!\n\nAlways with you, my master!\n`"
+                     f"`My peru owner`: {DEFAULTUSER}\n"
                      "[Deploy this userbot Now](https://github.com/gohilbapu/X-tra-Telegram)")
